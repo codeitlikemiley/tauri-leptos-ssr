338f5ef2ad3000583c379f02bd1abdd857a99cc2
codeitlikemiley <codeitlikemiley@gmail.com>
2026-05-29 14:07:47 +0800

    refactor: define LEPTOS_OUTPUT_NAME in .cargo/config.toml to remove WASM filename hack

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,15 +1,3 @@
 fn main() {
-    let wasm_path = std::path::Path::new("../target/site/pkg/app.wasm");
-    let bg_wasm_path = std::path::Path::new("../target/site/pkg/app_bg.wasm");
-    if wasm_path.exists() {
-        if let Err(e) = std::fs::rename(wasm_path, bg_wasm_path) {
-            println!(
-                "cargo:warning=Failed to rename app.wasm to app_bg.wasm: {}",
-                e
-            );
-        } else {
-            println!("cargo:info=Renamed app.wasm to app_bg.wasm");
-        }
-    }
     tauri_build::build();
 }
